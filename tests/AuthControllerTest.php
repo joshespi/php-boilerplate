8703908e3a5da7fb840c127e999e7fc7c63d336f
Josh Espinoza <joshespi@gmail.com>
2025-08-03 14:34:12 -0600

    removed redundant session start

--- tests/AuthControllerTest.php
+++ tests/AuthControllerTest.php
@@ -1,7 +1,7 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+// if (session_status() === PHP_SESSION_NONE) {
+//     session_start();
+// }
 require_once __DIR__ . '/TestDatabase.php';
 
 use PHPUnit\Framework\TestCase;
