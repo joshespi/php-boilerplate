7680748cbb8e51e41711017ced6a3a0920844f4f
Josh Espinoza <joshespi@gmail.com>
2025-08-03 18:16:06 -0600

    add test for duplicate username registration

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -17,4 +17,11 @@ class UserModelTest extends TestCase
         $this->assertNotEquals($plainPassword, $storedUser['password']);
         $this->assertTrue(password_verify($plainPassword, $storedUser['password']));
     }
+    public function testDuplicateUsernameRegistration()
+    {
+        $userModel = new User();
+        $userModel->create('uniqueuser', 'password1');
+        $result = $userModel->create('uniqueuser', 'password2');
+        $this->assertFalse($result, 'Should not allow duplicate usernames');
+    }
 }
